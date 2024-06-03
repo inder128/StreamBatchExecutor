c15342733faf672e768c9565bc55fba329801d72
Inderjeet Yadav <inder128.rao@gmail.com>
2024-06-03 16:13:25 +0530

    New repository using spring initializr

---
+++ src/main/java/com/example/StreamBatchExecutor/StreamBatchExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.StreamBatchExecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamBatchExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamBatchExecutorApplication.class, args);
+	}
+
+}
